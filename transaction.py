d8933e2bba4a97fca3d5af6dabe9b2b342a54515
Johnie <31553113+giannisgrigorakos@users.noreply.github.com>
2019-03-27 05:09:05 +0200

    Update transaction.py

--- transaction.py
+++ transaction.py
@@ -4,16 +4,20 @@ from utility.printable import Printable
 
 
 class Transaction(Printable):
-    """A transaction which can be added to a block in the blockchain."""
+    """A transaction which can be added to a block in the blockchain.
 
-    def __init__(self, txid, sender, recipient, signature, amount, inputs, outputs):
-        self.txid = txid
+    Attributes:
+        :sender: The sender of the coins.
+        :recipient: The recipient of the coins.
+        :signature: The signature of the transaction.
+        :amount: The amount of coins sent.
+    """
+
+    def __init__(self, sender, recipient, signature, amount):
         self.sender = sender
         self.recipient = recipient
-        self.amount = amount
         self.signature = signature
-        self.inputs = inputs
-        self.outputs = outputs
+        self.amount = amount
 
     def to_ordered_dict(self):
         """Converts this transaction into a (hashable) OrderedDict."""
